ff7a502df038a886065ea1afc4d43bdf4f3275c3
rsgc-chien-g <student@MacBook-Pro-0681-GChien.local>
2016-05-18 10:02:29 -0400

    Put in my URL
    replaced the url

--- CompletionHandlersAndAsynchronousRequests.playground/Contents.swift
+++ CompletionHandlersAndAsynchronousRequests.playground/Contents.swift
@@ -21,7 +21,7 @@ class ViewController : UIViewController {
             
             // Do the initial de-serialization
             // Source JSON is here:
-            // http://www.learnswiftonline.com/Samples/subway.json
+            // http://pokeapi.co/
             //
             let json = try NSJSONSerialization.JSONObjectWithData(theData, options: NSJSONReadingOptions.AllowFragments) as! AnyObject
             
@@ -95,7 +95,7 @@ class ViewController : UIViewController {
         }
         
         // Define a URL to retrieve a JSON file from
-        let address : String = "http://www.learnswiftonline.com/Samples/subway.json"
+        let address : String = "http://pokeapi.co/api/v2/pokemon/7/"
         
         // Try to make a URL request object
         if let url = NSURL(string: address) {
